4ded67195a6a2ec6e5ca35bb6ffc38cb9b93c36a
qinlu1024 <qinlu1024@users.noreply.github.com>
2023-10-16 09:18:07 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="qinlu1024"
+original_name="myfastapi"
+original_urlname="myfastapi"
+original_description="Awesome myfastapi created by qinlu1024"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv myfastapi $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
